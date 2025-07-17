5a3e228252a21055eae40c5678c7140dc6386ad8
Ho Tan Hung <tanhungho2002@gmail.com>
2025-07-17 07:48:06 +0700

    fix: apiURL

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
-const apiURL = "https://api-antoree.onrender.com";
+const apiURL =
+  process.env.NODE_ENV === "production"
+    ? "https://api-antoree.onrender.com"
+    : "http://localhost:3000/";
 const api = axios.create({
   baseURL: apiURL,
   headers: {
